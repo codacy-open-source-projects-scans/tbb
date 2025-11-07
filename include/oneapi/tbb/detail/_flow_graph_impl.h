715696ea062cbc62aab613e8639c67de43c03ca2
Codacy Archive <codacy-archive@local>
2025-11-07 15:50:43 +0000

    snapshot 2025-11-07T15:50:43.684558+00:00

--- include/oneapi/tbb/detail/_flow_graph_impl.h
+++ include/oneapi/tbb/detail/_flow_graph_impl.h
@@ -1,5 +1,6 @@
 /*
-    Copyright (c) 2005-2024 Intel Corporation
+    Copyright (c) 2005-2025 Intel Corporation
+    Copyright (c) 2025 UXL Foundation Contributors
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -146,8 +147,6 @@ private:
     friend graph_task* prioritize_task(graph& g, graph_task& gt);
 };
 
-inline bool is_this_thread_in_graph_arena(graph& g);
-
 #if __TBB_PREVIEW_FLOW_GRAPH_TRY_PUT_AND_WAIT
 class trackable_messages_graph_task : public graph_task {
 public:
@@ -160,12 +159,7 @@ public:
         auto last_iterator = my_msg_reference_vertices.cbefore_begin();
 
         for (auto& msg_waiter : my_msg_wait_context_vertices) {
-            // If the task is created by the thread outside the graph arena, the lifetime of the thread reference vertex
-            // may be shorter that the lifetime of the task, so thread reference vertex approach cannot be used
-            // and the task should be associated with the msg wait context itself
-            d1::wait_tree_vertex_interface* ref_vertex = is_this_thread_in_graph_arena(g) ?
-                                                         r1::get_thread_reference_vertex(msg_waiter) :
-                                                         msg_waiter;
+            d1::wait_tree_vertex_interface* ref_vertex = r1::get_thread_reference_vertex(msg_waiter);
             last_iterator = my_msg_reference_vertices.emplace_after(last_iterator,
                                                                     ref_vertex);
             ref_vertex->reserve(1);
@@ -420,7 +414,6 @@ private:
     friend void activate_graph(graph& g);
     friend void deactivate_graph(graph& g);
     friend bool is_graph_active(graph& g);
-    friend bool is_this_thread_in_graph_arena(graph& g);
     friend graph_task* prioritize_task(graph& g, graph_task& arena_task);
     friend void spawn_in_graph_arena(graph& g, graph_task& arena_task);
     friend void enqueue_in_graph_arena(graph &g, graph_task& arena_task);
@@ -453,13 +446,8 @@ inline graph_task::graph_task(graph& g, d1::small_object_allocator& allocator,
     , priority(node_priority)
     , my_allocator(allocator)
 {
-    // If the task is created by the thread outside the graph arena, the lifetime of the thread reference vertex
-    // may be shorter that the lifetime of the task, so thread reference vertex approach cannot be used
-    // and the task should be associated with the graph wait context itself
-    // TODO: consider how reference counting can be improved for such a use case. Most common example is the async_node
     d1::wait_context_vertex* graph_wait_context_vertex = &my_graph.get_wait_context_vertex();
-    my_reference_vertex = is_this_thread_in_graph_arena(g) ? r1::get_thread_reference_vertex(graph_wait_context_vertex)
-                                                           : graph_wait_context_vertex;
+    my_reference_vertex = r1::get_thread_reference_vertex(graph_wait_context_vertex);
     __TBB_ASSERT(my_reference_vertex, nullptr);
     my_reference_vertex->reserve();
 }
@@ -513,11 +501,6 @@ inline bool is_graph_active(graph& g) {
     return g.my_is_active;
 }
 
-inline bool is_this_thread_in_graph_arena(graph& g) {
-    __TBB_ASSERT(g.my_task_arena && g.my_task_arena->is_active(), nullptr);
-    return r1::execution_slot(*g.my_task_arena) != d1::slot_id(-1);
-}
-
 inline graph_task* prioritize_task(graph& g, graph_task& gt) {
     if( no_priority == gt.priority )
         return &gt;
@@ -526,7 +509,10 @@ inline graph_task* prioritize_task(graph& g, graph_task& gt) {
     //! priority queue, and a new critical task is created to take and execute a work item with
     //! the highest known priority. The reference counting responsibility is transferred to
     //! the new task.
-    d1::task* critical_task = gt.my_allocator.new_object<priority_task_selector>(g.my_priority_queue, gt.my_allocator);
+    // A newly created small_object_allocator should be used to allocate the priority_task_selector
+    // instead of the allocator, associated with gt since gt can be allocated by another thread
+    d1::small_object_allocator allocator;
+    d1::task* critical_task = allocator.new_object<priority_task_selector>(g.my_priority_queue, allocator);
     __TBB_ASSERT( critical_task, "bad_alloc?" );
     g.my_priority_queue.push(&gt);
     using tbb::detail::d1::submit;
@@ -543,7 +529,7 @@ inline void spawn_in_graph_arena(graph& g, graph_task& arena_task) {
 
         __TBB_ASSERT(g.my_task_arena && g.my_task_arena->is_active(), nullptr);
         submit( *gt, *g.my_task_arena, *g.my_context
-#if __TBB_PREVIEW_CRITICAL_TASKS
+#if __TBB_CRITICAL_TASKS
                 , /*as_critical=*/false
 #endif
         );
--- include/oneapi/tbb/flow_graph.h
+++ include/oneapi/tbb/flow_graph.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2005-2024 Intel Corporation
+    Copyright (c) 2005-2025 Intel Corporation
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -85,7 +85,7 @@ class continue_msg {};
 } // namespace d2
 
 #if __TBB_CPP20_CONCEPTS_PRESENT
-namespace d0 {
+inline namespace d0 {
 
 template <typename ReturnType, typename OutputType>
 concept node_body_return_type = std::same_as<OutputType, tbb::detail::d2::continue_msg> ||
@@ -127,7 +127,7 @@ template <typename Body, typename Input, typename GatewayType>
 concept async_node_body = std::copy_constructible<Body> &&
                           std::invocable<Body&, const Input&, GatewayType&>;
 
-} // namespace d0
+} // inline namespace d0
 #endif // __TBB_CPP20_CONCEPTS_PRESENT
 
 namespace d2 {
@@ -138,6 +138,7 @@ template< typename T > class receiver;
 class continue_receiver;
 
 template< typename T, typename U > class limiter_node;  // needed for resetting decrementer
+template< typename T > class overwrite_node; // needed for the forward friend declaration to work in GCC < 8
 
 template<typename T, typename M> class successor_cache;
 template<typename T, typename M> class broadcast_cache;
@@ -329,6 +330,7 @@ protected:
     virtual graph& graph_reference() const = 0;
 
     template<typename TT, typename M> friend class successor_cache;
+    template< typename TTT > friend class overwrite_node;
     virtual bool is_continue_receiver() { return false; }
 
     // TODO revamp: reconsider the inheritance and move node priority out of receiver
@@ -3318,6 +3320,12 @@ public:
         spin_mutex::scoped_lock l( my_mutex );
         if (my_buffer_is_valid && is_graph_active( my_graph )) {
             // We have a valid value that must be forwarded immediately.
+            if (s.is_continue_receiver()) {
+                // try_put can never fail, since continue_receivers always accept
+                my_successors.register_successor( s );
+                s.try_put( my_buffer );
+                return true;
+            }
             bool ret = s.try_put( my_buffer );
             if ( ret ) {
                 // We add the successor that accepted our put
